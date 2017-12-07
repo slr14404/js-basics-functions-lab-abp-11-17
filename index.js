a890471f1c726ba094deef996facf419a333e122
Stephanie D <slr14404@yahoo.com>
2017-12-07 02:09:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+funtion distanceFromHqInBlocks (block) {
+  if (block) > 42) {
+  return (block - 42);
+}
+  else {
+    (42 - block)
+  }
+}
+
+function distanceFromHqInFeet(block) {
+  return (distanceFromHqInFeet * 264)
+}
