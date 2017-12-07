76cd7be0b9197a3580ca43e3adb225ff64354884
Stephanie D <slr14404@yahoo.com>
2017-12-07 02:27:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blockNumber){
- -  if(blockNumber > 42){
- -      return blockNumber - 42
- +function distanceFromHqInBlocks (blockNumber) {
- +  if (blockNumber > 42) {
- +    return blockNumber - 42;
+  if(blockNumber > 42){
+    return blockNumber - 42
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+      return blockNumber - 42;
     } else {
- -      return 42 - blockNumber
- +    return 42 - blockNumber;
+      return 42 - blockNumber
+      return 42 - blockNumber;
     }
   }
   
@@ -43,10 +43,10 @@ function distanceFromHqInBlocks(blockNumber){
  +  } else if (distance > 400 && distance <= 2000) {
  +    return .02 * distance;
     } else if (distance > 2000 && distance < 2500) {
- -    return 25
- +    return 25;
-    } else {
- -    return 'cannot travel that far'
- +    return 'cannot travel that far';
+  return 25
+  return 25;
+} else {
+    return 'cannot travel that far'
+    return 'cannot travel that far';
     }
   }
